8372dd521b06a13dd369658af4a48b44db154c53
Katarzyna <katarzyna.kopowska@gmail.com>
2019-12-04 09:33:37 +0100

    Rename Scraper class to ServicesScraperManager

---
+++ exercise_1/job_scraper.py
@@ -0,0 +1,65 @@
+import requests
+import collections
+
+
+JobOffer = collections.namedtuple(
+    "JobOffer", ("position", "company", "url", "salary", "source"),
+)
+
+
+class NoFluffJobsScraper:
+    url = "https://nofluffjobs.com/api/search/posting"
+    data_class = JobOffer
+
+    def get_offers(self):
+        return requests.get(self.url).json()["postings"]
+
+    def parse_offer(self, offer):
+        data = {
+            "position": offer.get("title"),
+            "company": offer.get("name"),
+            "url": offer.get("url"),
+            "source": "nofluffjobs",
+            "salary": "-",
+        }
+
+        return self.data_class(**data)
+
+
+class JustJoinItScraper:
+    url = "https://justjoin.it/api/offers"
+    data_class = JobOffer
+
+    def get_offers(self):
+        return requests.get(self.url).json()
+
+    def parse_offer(self, offer):
+        data = {"position": offer.get("title"),
+                "company": offer.get("company_name"),
+                "salary": "{} - {}".format(offer.get("salary_from"), offer.get("salary_to")),
+                "url": "-",
+                "source": "justjoinit",
+                }
+
+        return self.data_class(**data)
+
+
+class ServicesScraperManager:
+    scraper_classes = (NoFluffJobsScraper, JustJoinItScraper)
+
+    def __init__(self):
+        self.offers = []
+
+    def run(self, filter_position=None):
+        for scraper_class in self.scraper_classes:
+            scraper = scraper_class()
+            for offer_data in scraper.get_offers():
+                offer = scraper.parse_offer(offer_data)
+                if filter_position and filter_position in offer.position.lower():
+                    self.offers.append(offer)
+        return self.offers
+
+
+scraper = ServicesScraperManager()
+scraper.run(filter_position="python")
+
