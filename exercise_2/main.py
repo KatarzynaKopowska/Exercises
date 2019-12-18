b3ada097f122a92faf740fc81755c673b9a1eeb1
Katarzyna <katarzyna.kopowska@gmail.com>
2019-12-18 09:28:21 +0100

    Create HourlyWeatherApiClient class

--- exercise_2/main.py
+++ exercise_2/main.py
@@ -1,6 +1,6 @@
 import sys
 from openweather_api_client import CurrentWeatherApiClient
-
+from openweather_api_client import HourlytWeatherApiClient
 
 if __name__ == "__main__":
     try:
@@ -17,3 +17,8 @@ if __name__ == "__main__":
           f"temp: {current_weather.temperature}(°F), "
           f"pressure: {current_weather.pressure}(hPa)"
           )
+
+
+    weather1 = HourlytWeatherApiClient()
+    hourly_weather = weather1.get_weather(filter='id', value='524901', api_key='8141563408c821030aa7cdacd8bd0eff')
+    print(hourly_weather)
--- exercise_2/openweather_api_client.py
+++ exercise_2/openweather_api_client.py
@@ -8,8 +8,7 @@ WeatherForecast = collections.namedtuple(
 
 
 class BasicWeatherApiClient:
-    """Basic abstraction for services to handle serializing data from service
-      and wrap them around data class.
+    """
     """
     data_class = None
 
@@ -23,6 +22,7 @@ class CurrentWeatherApiClient(BasicWeatherApiClient):
 
     def get_weather(self, filter=None, value=None, api_key=None):
         url = "http://api.openweathermap.org/data/2.5/weather?{}={}&APPID={}".format(filter, value, api_key)
+
         response = requests.get(url).json()
         data = {
             "city": response.get("name"),
@@ -32,4 +32,23 @@ class CurrentWeatherApiClient(BasicWeatherApiClient):
             "pressure": response["main"].get("pressure"),
         }
 
+        return self.data_class(**data)
+
+
+class HourlytWeatherApiClient(BasicWeatherApiClient):
+    data_class = WeatherForecast
+
+    def get_weather(self, filter=None, value=None, api_key=None):
+        url = "http://api.openweathermap.org/data/2.5/forecast?{}={}&APPID={}".format(filter, value, api_key)
+
+        city_information = requests.get(url).json()["city"]
+        weather_information = requests.get(url).json()["list"][0]
+        data = {
+            "city": city_information.get("name"),
+            "country": city_information.get("country"),
+            "current_weather": weather_information["weather"][0].get("main"),
+             "temperature": weather_information["main"].get("temp"),
+             "pressure": weather_information["main"].get("pressure"),
+        }
+        print(data)
         return self.data_class(**data)
