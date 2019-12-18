5795875fb9526b5cf904f63d9195b2eeb6946542
Katarzyna <katarzyna.kopowska@gmail.com>
2019-12-18 10:01:23 +0100

    Create WeatherForecastManager class to manage OpenWather API Client classes

--- exercise_2/openweather_api_client.py
+++ exercise_2/openweather_api_client.py
@@ -3,7 +3,7 @@ import collections
 
 
 WeatherForecast = collections.namedtuple(
-    "WeatherForecast", ("city", "country", "current_weather", "temperature", "pressure")
+    "WeatherForecast", ("weather_forecast_type", "city", "country", "current_weather", "temperature", "pressure")
     )
 
 
@@ -20,35 +20,52 @@ class BasicWeatherApiClient:
 class CurrentWeatherApiClient(BasicWeatherApiClient):
     data_class = WeatherForecast
 
-    def get_weather(self, filter=None, value=None, api_key=None):
+    def get_weather(self, filter, value, api_key):
         url = "http://api.openweathermap.org/data/2.5/weather?{}={}&APPID={}".format(filter, value, api_key)
-
-        response = requests.get(url).json()
+        weather_information = requests.get(url).json()
         data = {
-            "city": response.get("name"),
-            "country": response["sys"].get("country"),
-            "current_weather": response["weather"][0].get("main"),
-            "temperature": response["main"].get("temp"),
-            "pressure": response["main"].get("pressure"),
+            "weather_forecast_type": "Current weather",
+            "city": weather_information.get("name"),
+            "country": weather_information["sys"].get("country"),
+            "current_weather": weather_information["weather"][0].get("main"),
+            "temperature": weather_information["main"].get("temp"),
+            "pressure": weather_information["main"].get("pressure"),
         }
-
         return self.data_class(**data)
 
 
 class FiveDayForecastApiClient(BasicWeatherApiClient):
     data_class = WeatherForecast
 
-    def get_weather(self, filter=None, value=None, api_key=None):
+    def get_weather(self, filter, value, api_key):
         url = "http://api.openweathermap.org/data/2.5/forecast?{}={}&APPID={}".format(filter, value, api_key)
 
         city_information = requests.get(url).json()["city"]
         weather_information = requests.get(url).json()["list"][0]
         data = {
+            "weather_forecast_type": "5 days forecast",
             "city": city_information.get("name"),
             "country": city_information.get("country"),
             "current_weather": weather_information["weather"][0].get("main"),
-             "temperature": weather_information["main"].get("temp"),
-             "pressure": weather_information["main"].get("pressure"),
+            "temperature": weather_information["main"].get("temp"),
+            "pressure": weather_information["main"].get("pressure"),
         }
-
         return self.data_class(**data)
+
+
+class WeatherForecastManager:
+    weather_forecast_classes = [CurrentWeatherApiClient, FiveDayForecastApiClient]
+
+    def __init__(self):
+        self.weather_forecast = []
+
+    def run(self, filter=None, value=None, api_key=None):
+        for weather_forecast_class in self.weather_forecast_classes:
+            weather_api_client = weather_forecast_class()
+            weather = weather_api_client.get_weather(filter, value, api_key)
+            self.weather_forecast.append(weather)
+        return self.weather_forecast
+
+
+
+
